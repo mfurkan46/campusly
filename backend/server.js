1700ebef16e71bb59960985fc4fe7d2a1e92d07b
Muhammed Furkan Demirci <58640766+mfurkan46@users.noreply.github.com>
2025-04-13 14:36:45 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -29,6 +29,8 @@ app.use(cookieParser());
 app.use(cors({
   origin: 'https://campusly-pi.vercel.app',
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
 app.use('/api/auth', authRoutes);
